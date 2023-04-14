e970348355f2a3509c2b2d49232aade4addfa372
Matheus Fragasse <kuhora@gmail.com>
2023-04-14 20:21:36 -0300

    Final Project

--- script.js
+++ script.js
@@ -61,3 +61,23 @@ function initScrollSmooth(){
 }
 
 initScrollSmooth();
+
+function initAnimaScroll() { 
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll(){
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const isSectionVisible = (sectionTop - windowMetade) < 0
+        if(isSectionVisible){
+          section.classList.add('ativo');
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimaScroll();
